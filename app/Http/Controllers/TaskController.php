27cf4f22fc96f732248a264d20656848cf5353b2
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-12-10 20:19:18 +0800

    更正"限制有關TaskController的路由只能由有經過驗證個使用者存取"，增加回傳tasks.index視圖程式碼

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index(Request $request){
+        return view('tasks.index');
+    }
+    public function __construct()
+    {
+        $this->middleware('auth');//讓有經過驗證的使用者才能存取
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -14,9 +14,4 @@ class Task extends Model
     {
         return $this->belongsTo(User::class);//與User model的關聯
     }
-
-    public function __construct()
-    {
-        $this->middleware('auth');//讓有經過驗證的使用者才能存取
-    }
 }
