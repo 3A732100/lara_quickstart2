12d76eedd9d7884f38728462553d3066b879da27
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-12-11 11:30:25 +0800

    顯示已有的任務

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Task;
 class TaskController extends Controller
 {
     public function index(Request $request){
-        return view('tasks.index');//回傳tasks.index視圖
+        $tasks = Task::where('user_id', $request->user()->id)->get();
+        //或$tasks= auth()->user()->tasks;
+        //$tasks= auth()->user()->tasks()->get();
+        //$tasks=Auth::user()->tasks;
+        //$tasks=Auth::user()->tasks()->get();
+
+        return view('tasks.index', [
+            'tasks' => $tasks,//回傳tasks.index視圖,並顯示送出request的使用者的所有tasks
+        ]);
     }
 
     public function store(Request $request)
@@ -19,7 +27,8 @@ class TaskController extends Controller
         $request->user()->tasks()->create([//從送出request的人的User model找到其Task model,對tasks資料表進行create的動作
             'name' => $request->name,//*送出request的人所輸入的name會被寫入tasks資料表的name
         ]);
-
+        //$request->user()->tasks()->create( $request->all() ); $request->all()是將送出request的人所輸入的所有資料,寫入tasks資料表對應欄位
+        //auth()->user()->tasks()->create( $request->all() ); $request->user()=auth()->user()=Auth:: user()
         return redirect('/tasks');//跳轉至/tasks(tasks.index)
 
     }
