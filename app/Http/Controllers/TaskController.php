bfae83f7fb379beb1f3c9fa42226397ac181aef7
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-12-11 10:32:58 +0800

    TaskController加入store時的驗證條件

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,6 +9,14 @@ class TaskController extends Controller
     public function index(Request $request){
         return view('tasks.index');//回傳tasks.index視圖
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',//寫入時,驗證資料是否非空白,且字數不可大於255字元
+        ]);
+    }
+
     public function __construct()
     {
         $this->middleware('auth');//讓有經過驗證的使用者才能存取
