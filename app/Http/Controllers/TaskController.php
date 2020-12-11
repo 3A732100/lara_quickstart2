57eeaa1ca24515a00bf0e17312f33f11e9ea293e
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-12-11 10:53:35 +0800

    建立新任務,建立後跳轉頁面

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,6 +15,13 @@ class TaskController extends Controller
         $this->validate($request, [
             'name' => 'required|max:255',//寫入時,驗證資料是否非空白,且字數不可大於255字元
         ]);
+
+        $request->user()->tasks()->create([//從送出request的人的User model找到其Task model,對tasks資料表進行create的動作
+            'name' => $request->name,//*送出request的人所輸入的name會被寫入tasks資料表的name
+        ]);
+
+        return redirect('/tasks');//跳轉至/tasks(tasks.index)
+
     }
 
     public function __construct()
